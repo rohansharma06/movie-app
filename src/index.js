3157014b9c2d84aca1fbe41bcd4de54f4bd8270c
rohansharma06 <rohansharma251998@gmail.com>
2020-07-03 07:21:21 +0530

    update middleware

--- src/index.js
+++ src/index.js
@@ -10,25 +10,24 @@ import rootReducer from './reducers';
 // function logger(obj,next,action)
 //logger(obj)(next)(action)
 //-- obj ={dispatch, getState}
-const logger = function({ dispatch, getState}){
-    return function(next){
-        return function(action){
-            console.log("ACTION_TYPE:",action.type);
-            next(action); //-- pass to another MW if present else go to dispatch
-        }
-    }
+//--- using curring
+
+    // const logger = function({ dispatch, getState}){
+    //     return function(next){
+    //         return function(action){
+    //             console.log("ACTION_TYPE:",action.type);
+    //             next(action); //-- pass to another MW if present else go to dispatch
+    //         }
+    //     }
+    // }
+
+//--- diff to call function
+const logger = ({ dispatch, getState}) => (next) => (action) => {
+    console.log("ACTION_TYPE:",action.type);
+    next(action); //-- pass to another MW if present else go to dispatch
 }
 
 const store = createStore(rootReducer, applyMiddleware(logger));
-//console.log('store:',store);
-// console.log('BEFORE STATE:',store.getState());
-
-// //---- dispatch send action to reducer
-// store.dispatch({
-//   type: 'ADD_MOVIES',
-//   movies: [{ name: 'Superman'}]
-// })
-// console.log('AFTER STATE:',store.getState());
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
 
